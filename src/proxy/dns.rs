e0f84a70cc5e2b81d9635bbf91afee588f893c6d
woofwoof <zonevon.b@gmail.com>
2025-04-27 06:11:58 +0700

    Update dns.rs

--- src/proxy/dns.rs
+++ src/proxy/dns.rs
@@ -2,16 +2,24 @@ use anyhow::Result;
 use reqwest::header::{HeaderMap, HeaderValue, ACCEPT, CONTENT_TYPE};
 use reqwest::Client;
 
-pub async fn doh(req_wireformat: &[u8]) -> Result<Vec<u8>> {
+pub async fn doh(req_wireformat: &[u8], use_cloudflare: bool) -> Result<Vec<u8>> {
     let mut headers = HeaderMap::new();
     headers.insert(
         CONTENT_TYPE,
         HeaderValue::from_static("application/dns-message"),
     );
     headers.insert(ACCEPT, HeaderValue::from_static("application/dns-message"));
+    
+    // Pilih URL berdasarkan parameter `use_cloudflare`
+    let url = if use_cloudflare {
+        "https://1.1.1.1/dns-query" // Cloudflare DNS-over-HTTPS
+    } else {
+        "https://8.8.8.8/dns-query" // Google DNS-over-HTTPS
+    };
+
     let client = Client::new();
     let response = client
-        .post("https://1.1.1.1/dns-query")
+        .post(url)
         .headers(headers)
         .body(req_wireformat.to_vec())
         .send()
