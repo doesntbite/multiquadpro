f3abd1febc6a8e86bf12c73237b93e1bef8ab7ad
woofwoof <zonevon.b@gmail.com>
2025-04-28 05:57:16 +0700

    Update dns.rs

--- src/proxy/dns.rs
+++ src/proxy/dns.rs
@@ -1,5 +1,4 @@
-use anyhow::Result;
-use reqwest::header::{HeaderMap, HeaderValue, ACCEPT, CONTENT_TYPE};
+use futures_util::future::select_ok;
 use reqwest::Client;
 
 pub async fn doh(req_wireformat: &[u8]) -> Result<Vec<u8>> {
@@ -9,15 +8,23 @@ pub async fn doh(req_wireformat: &[u8]) -> Result<Vec<u8>> {
         HeaderValue::from_static("application/dns-message"),
     );
     headers.insert(ACCEPT, HeaderValue::from_static("application/dns-message"));
-    let client = Client::new();
-    let response = client
-        .post("https://8.8.8.8/dns-query")
-        .headers(headers)
-        .body(req_wireformat.to_vec())
-        .send()
-        .await?
-        .bytes()
-        .await?;
 
-    Ok(response.to_vec())
+    let providers = [
+        "https://1.1.1.1/dns-query",
+        "https://8.8.8.8/dns-query",
+        "https://dns.google/dns-query",
+    ];
+
+    let client = Client::new();
+    let requests = providers.iter().map(|&provider| {
+        client
+            .post(provider)
+            .headers(headers.clone())
+            .body(req_wireformat.to_vec())
+            .send()
+    });
+
+    let result = select_ok(requests).await?;
+    let bytes = result.0.bytes().await?;
+    Ok(bytes.to_vec())
 }
