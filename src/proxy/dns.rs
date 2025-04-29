e9f6002f40c9244542051137ac72a2cec2f25c69
woofwoof <zonevon.b@gmail.com>
2025-04-30 06:53:58 +0700

    Update dns.rs

--- src/proxy/dns.rs
+++ src/proxy/dns.rs
@@ -1,31 +1,21 @@
 use anyhow::Result;
-use reqwest::header::{HeaderMap, HeaderValue, ACCEPT, CONTENT_TYPE, USER_AGENT};
+use reqwest::header::{HeaderMap, HeaderValue, ACCEPT, CONTENT_TYPE};
 use reqwest::Client;
 
-pub async fn doh(req_wireformat: &[u8], endpoint: Option<&str>) -> Result<Vec<u8>> {
-    // Prepare DNS-over-HTTPS headers
+pub async fn doh(req_wireformat: &[u8]) -> Result<Vec<u8>> {
     let mut headers = HeaderMap::new();
     headers.insert(
         CONTENT_TYPE,
         HeaderValue::from_static("application/dns-message"),
     );
     headers.insert(ACCEPT, HeaderValue::from_static("application/dns-message"));
-    headers.insert(USER_AGENT, HeaderValue::from_static("rust-doh-client/1.0"));
-
-    // Create HTTP client
     let client = Client::new();
-
-    // Use provided endpoint or default to Cloudflare
-    let endpoint = endpoint.unwrap_or("https://cloudflare-dns.com/dns-query");
-
-    // Send POST request to DoH endpoint
     let response = client
-        .post(endpoint)
+        .post("https://cloudflare-dns.com/dns-query")
         .headers(headers)
         .body(req_wireformat.to_vec())
         .send()
         .await?
-        .error_for_status()?
         .bytes()
         .await?;
 
