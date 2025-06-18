a07461945fd6560d6323bba5dae40b9ea2e9cd23
woofwoof <zonevon.b@gmail.com>
2025-06-18 14:06:38 +0700

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,3 +1,5 @@
+#!/bin/bash
+
 for i in {1..2}; do
   name="siren$i"
   domain="zxc$i.sumbangsih.dpdns.org"
@@ -6,11 +8,15 @@ for i in {1..2}; do
 
   cp wrangler.toml.bak wrangler.temp.toml
 
-  # Ganti nama worker
+  # Ganti name di bagian atas
   sed -i "s/^name = \".*\"/name = \"$name\"/" wrangler.temp.toml
 
-  # Tambahkan custom domain
-  echo -e "\nroutes = [\n  { pattern = \"$domain\", custom_domain = true }\n]" >> wrangler.temp.toml
+  # Ganti baris routes yang sudah ada, atau tambahkan jika belum ada
+  if grep -q "^routes = " wrangler.temp.toml; then
+    sed -i "s|^routes = .*|routes = [\n  { pattern = \"$domain\", custom_domain = true }\n]|" wrangler.temp.toml
+  else
+    echo -e "\nroutes = [\n  { pattern = \"$domain\", custom_domain = true }\n]" >> wrangler.temp.toml
+  fi
 
   # Deploy
   wrangler deploy --config wrangler.temp.toml
